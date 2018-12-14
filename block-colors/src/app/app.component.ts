16c8d4ddfcf0846969c885d3c9ad026b5e86e4d2
George Spasov <spasov05203@gmail.com>
2018-12-14 12:32:39 +0100

    Website

---
+++ block-colors/src/app/app.component.ts
@@ -0,0 +1,46 @@
+declare let require: any;
+import { Component } from '@angular/core';
+import * as ethers from 'ethers';
+const CryptoColor = require('./contract_interfaces/CryptoColor.json');
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  public tokenId: string;
+  public infuraApiKey = 'abca6d1110b443b08ef271545f24b80d';
+  public infuraProvider: ethers.providers.InfuraProvider;
+  public contractAddress = '0x41A32C610FA23dEa9E85D471CAD66ea828853153';
+  public deployedContract: ethers.Contract;
+  public tokensCount = 0;
+  public tokenData = {
+    r: 255,
+    g: 255,
+    b: 255
+  };
+
+  constructor() {
+
+    this.infuraProvider = new ethers.providers.InfuraProvider('ropsten', this.infuraApiKey);
+    this.deployedContract = new ethers.Contract(this.contractAddress, CryptoColor.abi, this.infuraProvider);
+
+  }
+
+  public async getTokensCount() {
+    this.tokensCount = await this.deployedContract.totalSupply();
+  }
+
+  public async getTokenColor() {
+    const colors = await this.deployedContract.getColorFromId(this.tokenId);
+    console.log(colors);
+    this.tokenData = {
+      r: colors[0],
+      g: colors[1],
+      b: colors[2]
+    };
+  }
+
+}
